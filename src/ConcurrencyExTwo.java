45aa3bf63d6a527d4076cf04e1f7dce42724ce81
Philipp Ressel <philipp.ressel@johner-institut.de>
2022-04-03 16:43:59 +0200

    first commit

---
+++ src/ConcurrencyExTwo.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+
+public class ConcurrencyExTwo {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        ProofOfWork pof = new ProofOfWork(0, "new block", new BigInteger("11990999998853575395644202399761444748041817389874403947722788198528885"));
+        long startTime = System.currentTimeMillis();
+        System.out.println(pof.proveWork());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
+    }
+}
---
+++ src/ProofOfWork.java
@@ -0,0 +1,39 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class ProofOfWork {
+    int nonce;
+    String block;
+    BigInteger target;
+    public ProofOfWork(int nonce, String block, BigInteger target) {
+        this.nonce = nonce;
+        this.block = block;
+        this.target = target;
+    }
+
+    public int proveWork() throws NoSuchAlgorithmException{
+        int nonceInner = this.nonce;
+        while (true) {
+            boolean result = checkHash(this.block, nonceInner, this.target);
+            if (result==true) {
+                break;
+            } else {
+                nonceInner++;
+            }
+        }
+        return nonceInner;
+    }
+
+    public boolean checkHash(String block, int nonce, BigInteger target) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(digest.digest(block.concat(Integer.toString(nonce)).getBytes(StandardCharsets.UTF_8)));
+        BigInteger number = new BigInteger(1, hash);
+        if (number.compareTo(target) == -1) {
+            return true;
+        } else return false;
+    }
+
+}
