8046c54bae559c2c9c76e31064560918f89e1f0e
Philipp Ressel <philipp.ressel@johner-institut.de>
2022-04-03 18:52:34 +0200

    concurrency_ex_part_2: thread added

--- src/ConcurrencyExTwo.java
+++ src/ConcurrencyExTwo.java
@@ -4,7 +4,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class ConcurrencyExTwo {
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        ProofOfWork pof = new ProofOfWork(0, "new block", new BigInteger("11990999998853575395644202399761444748041817389874403947722788198528885"));
+        ProofOfWork pof = new ProofOfWork(0, "new block", new BigInteger("7711990999998853575395644202399761444748041817389874403947722788198528885"));
         long startTime = System.currentTimeMillis();
         System.out.println(pof.proveWork());
         long endTime = System.currentTimeMillis();
--- src/ProofOfWork.java
+++ src/ProofOfWork.java
@@ -8,32 +8,54 @@ public class ProofOfWork {
     int nonce;
     String block;
     BigInteger target;
+    int winnerNonce;
     public ProofOfWork(int nonce, String block, BigInteger target) {
         this.nonce = nonce;
         this.block = block;
         this.target = target;
+        this.winnerNonce = -1;
     }
 
     public int proveWork() throws NoSuchAlgorithmException{
         int nonceInner = this.nonce;
         while (true) {
-            boolean result = checkHash(this.block, nonceInner, this.target);
-            if (result==true) {
+            new Thread(new Runnable() {
+                private String block;
+                private BigInteger target;
+                private int nonceInner;
+                public Runnable init(String block, BigInteger target, int nonceInner) {
+                    this.block = block;
+                    this.target = target;
+                    this.nonceInner = nonceInner;
+                    return this;
+                }
+                @Override
+                public void run() {
+                    try {
+                        checkHash(this.block, nonceInner, this.target);
+                        //System.out.println(nonceInner);
+                    }
+                    catch (NoSuchAlgorithmException e) {
+                        System.out.println("NoSuchAlgorithmException");
+                    }
+                }
+            }.init(this.block, this.target, nonceInner)).start();
+            if (winnerNonce != -1) {
                 break;
-            } else {
-                nonceInner++;
-            }
+            } else nonceInner++;
         }
-        return nonceInner;
+        return winnerNonce;
     }
 
-    public boolean checkHash(String block, int nonce, BigInteger target) throws NoSuchAlgorithmException {
+    public void checkHash(String block, int nonce, BigInteger target) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] hash = digest.digest(digest.digest(block.concat(Integer.toString(nonce)).getBytes(StandardCharsets.UTF_8)));
         BigInteger number = new BigInteger(1, hash);
+        System.out.println(number);
         if (number.compareTo(target) == -1) {
-            return true;
-        } else return false;
+            System.out.println("hello");
+            this.winnerNonce = nonce;
+        }
     }
 
 }
